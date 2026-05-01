9dd3ed43a698494ed13c634b207a0d575dfbdb08
tiltFrankie <frankie.denell@tilt.com>
2026-05-01 15:14:05 -0500

    render markdown plugin

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -12,6 +12,7 @@ vim.pack.add({
   { src = "https://github.com/windwp/nvim-ts-autotag" },
   { src = "https://github.com/windwp/nvim-autopairs" },
   { src = "https://github.com/christoomey/vim-tmux-navigator" },
+  { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 })
 
 require("plugins.rose-pine")
@@ -26,3 +27,4 @@ require("plugins.bufremove")
 require("plugins.ts-autotag")
 require("plugins.autopairs")
 require("plugins.tmux-navigator")
+require("plugins.render-markdown")
