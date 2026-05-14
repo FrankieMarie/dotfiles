d5b71907443fb716c28c02dbb38f2c4ae60385fc
tiltFrankie <frankie.denell@tilt.com>
2026-05-14 15:45:14 -0500

    rosebones

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,21 +1,5 @@
 vim.opt.background = "dark"
 
-vim.g.zenwritten_transparent_background = true
-vim.g.zenbones_transparent_background = true
+vim.g.rosebones_transparent_background = true
 
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = { "zenwritten", "zenbones" },
-  callback = function()
-    local pink = require(vim.g.colors_name .. ".palette").dark.blossom.hex
-    for _, g in ipairs({
-      "Operator", "String", "Character",
-      "@operator", "@punctuation.bracket", "@punctuation.delimiter",
-      "@tag.delimiter", "@string", "@string.escape",
-      "@string.special", "@string.regex", "@character",
-    }) do
-      vim.api.nvim_set_hl(0, g, { fg = pink })
-    end
-  end,
-})
-
-vim.cmd.colorscheme("zenwritten")
+vim.cmd.colorscheme("rosebones")
