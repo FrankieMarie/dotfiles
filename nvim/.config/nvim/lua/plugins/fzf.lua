447360141308f1f6f11691ff16be393fcd2ea8c9
FrankieMarie <frankiemarie83@gmail.com>
2026-05-21 18:28:24 -0500

    map fw and fW for fzf

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -4,6 +4,8 @@ require("fzf-lua").setup({})
 local map = vim.keymap.set
 map("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Find files" })
 map("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Live grep" })
+map("n", "<leader>fw", "<cmd>FzfLua grep_cword<cr>", { desc = "Grep word under cursor" })
+map("n", "<leader>fW", "<cmd>FzfLua grep_cWORD<cr>", { desc = "Grep WORD under cursor" })
 map("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
 map("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", { desc = "Help tags" })
 map("n", "<leader>fr", "<cmd>FzfLua resume<cr>", { desc = "Resume last search" })
